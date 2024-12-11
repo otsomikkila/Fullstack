74070f1adbbd5753549a18db586a0007148b7b7a
otsomikkila <otso.mikkila@hotmail.fi>
2024-12-11 13:41:36 +0200

    course information step 6

---
+++ part2/courseinformation/src/App.jsx
@@ -0,0 +1,78 @@
+const Header = (props) => {
+  return (
+    <h1>{props.name}</h1>
+  )
+}
+
+const Content = ({parts}) => {
+  console.log(parts[0])
+
+  return (
+    <div>
+      {parts.map(part => 
+        <Part part={part} key={part.id}/>
+      )}
+    </div>
+  )
+}
+
+const Part = ({part}) => {
+  //console.log(props.part)
+  return (
+    <div>
+      <p>{part.name} {part.exercises}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  let sum = 0
+  const partsList = props.parts.map(n => sum += n.exercises)
+  console.log(partsList)
+  console.log(sum)
+  return (
+    <p>Number of exercises {sum}</p>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+    <Header name={course.name}/>
+    <Content parts={course.parts}/>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+  
+  return (
+    <div>
+      <Course course={course}/>
+    </div>
+  )
+  
+}
+
+export default App
